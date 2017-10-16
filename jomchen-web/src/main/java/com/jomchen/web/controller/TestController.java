a5e6ce0c3bdc7e69090bc379af8953b34e83d26e
Jomchen <937574740@qq.com>
2017-10-16 14:04:18 +0800

    配置 git

--- jomchen-web/src/main/java/com/jomchen/web/controller/TestController.java
+++ jomchen-web/src/main/java/com/jomchen/web/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.jomchen.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Create by Jomchen on 15:11 2017/9/25
@@ -9,9 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
+    @Value("${personal.username}")
+    private String personalUsername;
+    @Value("${personal.password}")
+    private String personalPassword;
+
     @RequestMapping("/goTestPage")
     public String goTestPage() {
         return "/views/test/test";
+    }
+
+    @RequestMapping("/testGitConfig")
+    @ResponseBody
+    public String testGitConfig() {
+        return "{name: " + personalUsername + ", password: " + personalPassword + "}";
     }
 
 }
