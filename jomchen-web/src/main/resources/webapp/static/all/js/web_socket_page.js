84152c07da2cfcc80416c83f8eb085457bb181be
Jomchen <937574740@qq.com>
2017-10-18 15:27:26 +0800

    修改 websocket

--- jomchen-web/src/main/resources/webapp/static/all/js/web_socket_page.js
+++ jomchen-web/src/main/resources/webapp/static/all/js/web_socket_page.js
@@ -1,7 +1,21 @@
-var ws = new WebSocket("/spring-websocket-portfolio/myHandler");
-var stompClient = Stomp.over(socket);
-stompClient.connect({}, function() {
 
+
+$(function() {
+
+    var ws = new WebSocket("ws://127.0.0.1:8080/myHandler");
+    ws.onopen = function(event) {
+        console.log("正在进行连接 websocket 。。。");
+    };
+    ws.onerror = function(event) {
+        console.log("websocket 错误 。。。");
+    };
+    ws.onmessage = function(event) {
+        console.log("数据为：" + event.data);
+    };
+
+    $(".anniu").click(function() {
+        ws.send("Hello Linux !!" + new Date().getTime());
+    });
 });
 
 
